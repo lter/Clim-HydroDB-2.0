d059d1f4c2c1fbd966b2ce1a65163d4ae32d44d6
Susanne Grossman-Clarke <grossmanclar@wisc.edu>
2021-08-09 13:12:08 -0400

    Update convert2odm_and_single_sensor.R

--- ODM_CSV_Examples/AND/AND3222/R_scripts/convert2odm_and_single_sensor.R
+++ ODM_CSV_Examples/AND/AND3222/R_scripts/convert2odm_and_single_sensor.R
@@ -1,7 +1,7 @@
-# Convert subset of package: knb-lter-and.3222.36 into CUAHSI ODM format
+# Convert subset of data package knb-lter-and.3222.36 into CUAHSI ODM format
 # Data set title: "Meteorological data from benchmark stations at the Andrews Experimental Forest, 1957 to present."
-# Subset: Table MS00111.txt and PROBE_CODE "AIRCEN08" (5 minute air temperatures)
-# Average 5 minute data to 15 minutes for ODM format
+# Subset: use Table MS00111.txt and select air temperatures from benchmark station PRIMET at 350 cm height
+# Air temperature values in 15 min intervals
 
 library(tidyverse) 
 library(lubridate)
@@ -9,73 +9,66 @@ library(chunked)
 
 ## specify inputs
 
-    dir_data <- "data"
+    dir_data <- "./data"
     data_table_name <- "MS00111.txt"
-    probe_code <- "AIRCEN08"
+    site_code <- "PRIMET"
     UTCOffset <- -8 # to Pacific Standard Time
-    dir_odm_tables <- "odm_tables"
+    dir_odm_tables <- "./odm_tables"
 
-## run this code section only once to:
-#  - create separate data file from MS00111.txt for PROBE_CODE "AIRCEN08.csv"
-#  - determine unique AIRTEMP_METHOD codes for ODM table "methods.csv"
+## run code section only once to
+#  - create separate data file from MS00111.txt for site_code "PRIMET" and height "350"
+#  - determine AIRTEMP_METHOD codes for ODM table "methods.csv"
 #  - use package chunk for chunkwise reading of large data file and apply dplyr commands to each chunk
 #
+#    
 #    in_file <- file.path(dir_data,data_table_name)
-#    read_table_chunkwise(file=in_file, header = TRUE, sep = ",") %>%
-#        filter(PROBE_CODE==probe_code) %>%
-#        write_chunkwise(file=file.path(dir_data, paste(probe_code, "csv", sep=".")))
+#    read_table_chunkwise(file=in_file,header = TRUE, sep = ",") %>%
+#    filter(SITECODE == "PRIMET") %>%
+#    filter(HEIGHT == "350") %>%
+#    write_chunkwise(file=file.path(dir_data, paste(site_code, "csv", sep=".")))
 #
-#    df_met <- read_csv(file=file.path(dir_data, paste(probe_code, "csv", sep=".")))
-#    method_code <- unique(df_met$AIRTEMP_METHOD)
+#    df_met <- read_csv(file=file.path(dir_data, paste(site_code, "csv", sep=".")))
+#    method_code <- unique(df_met$AIRTEMP_METHOD) #edit Methods.csv table accordingly
+#    
 ## end section
-    
-# read information from ODM tables and assign to columns of "DataValues.csv"
+
+# read air temperature data from data file and assign column "DataValue"
+    df_met <- read_csv(file=file.path(dir_data, paste(site_code, "csv", sep=".")))
+    df_temp <- select(df_met,DataValue=AIRTEMP_MEAN)
+
+# read information from ODM tables that will be addded as columns to ODM table "DataValues.csv"
 
     odm_table_sources <- read_csv(file.path(dir_odm_tables, "Sources.csv"))
     SourceCode <- odm_table_sources$SourceCode #"AND"
 
     odm_table_sites <- read_csv(file.path(dir_odm_tables, "Sites.csv"))
-    SiteCode <- odm_table_sites$SiteCode[1]  #"AIRCEN08"
+    SiteCode <- odm_table_sites$SiteCode[2]  #"PRIMET"
 
     odm_table_variables <- read_csv(file.path(dir_odm_tables, "Variables.csv"))
     VariableCode <- odm_table_variables$VariableCode[1]   #"AIRTEMP_MEAN"
-# or assign directly
-#df_odm$SourceCode <- "AND"
-#df_odm$SiteCode <- "AIRCEN08"
-#df_odm$SiteCode <- "AIRTEMP_MEAN"
-
-# read original data
-    df_met <- read_csv(file=file.path(dir_data, paste(probe_code, "csv", sep=".")))
-
-# determine 15 minute time intervals
-# drop the records that didn't parse into datetime
-    df_met <- drop_na(df_met, DATE_TIME)
-# 15 minute intervals and add column to tibble df_met
-    df_met$LocalDateTime <- cut(df_met$DATE_TIME, breaks='15 min')
-# assign type date/time
-    df_met$LocalDateTime <- ymd_hms(df_met$LocalDateTime)
-
-# calculate 15 minute averages of 5 minute air temperature values and assign Method and Flag
-    df_temp_mean <- df_met %>% group_by(LocalDateTime) %>% summarize(DataValue = mean(AIRTEMP_MEAN))
-    df_mean_MethodCode <- df_met %>% group_by(LocalDateTime) %>% summarize(MethodCode = first(AIRTEMP_METHOD))
-    df_mean_QualityControlLevelCode <- df_met %>% group_by(LocalDateTime) %>% summarize(QualityControlLevelCode = first(AIRTEMP_MEAN_FLAG))    
-
-# Make ODM table "DataValues.csv"
-    df_odm <-  df_temp_mean %>% mutate(MethodCode = df_mean_MethodCode$MethodCode,
-               QualityControlLevelCode = df_mean_QualityControlLevelCode$QualityControlLevelCode,
-               UTCOffset,
-               DateTimeUTC = LocalDateTime + hours(-UTCOffset),
-               SiteCode,
-               VariableCode,
-               SourceCode,
-               QualityControlLevelCode)
+    
+    QualityControlLevelCode = 1
+    
+    df_met_QualityCode <- df_met$AIRTEMP_MEAN_FLAG
+    df_temp_MethodCode <- df_met$AIRTEMP_METHOD     #Should it be probe_code?
+    
+# make ODM table "DataValues.csv"
+    df_odm <- df_temp %>% mutate(LocalDateTime = df_met$DATE_TIME,
+                      UTCOffset,
+                      DateTimeUTC = df_met$DATE_TIME + hours(-UTCOffset),
+                      SiteCode,
+                      VariableCode,
+                      SourceCode,
+                      MethodCode = df_temp_MethodCode,
+                      QualityControlLevelCode = 1,
+                      QualifierCode = df_met_QualityCode)
     
 # replace missing values NA with -9999 
     df_odm$DataValue[is.na(df_odm$DataValue)] <- -9999
 
 # re-order columns
-    df_odm <- select(df_odm, DataValue, LocalDateTime, UTCOffset, DateTimeUTC, SiteCode, VariableCode, MethodCode, SourceCode, QualityControlLevelCode)
+    df_odm <- select(df_odm, DataValue, LocalDateTime, UTCOffset, DateTimeUTC, SiteCode, VariableCode, MethodCode, SourceCode, QualityControlLevelCode, QualifierCode)
     
 # write "DataValues.csv"
-    out_file <- file.path(dir_odm_tables,paste(probe_code,"DataValues.csv", sep="_"))
+    out_file <- file.path(dir_odm_tables,paste(site_code,"DataValues.csv", sep=","))
     write_csv(df_odm,out_file)
